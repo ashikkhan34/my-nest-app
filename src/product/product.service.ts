8f0c2d66b22ddb400220d5134f0a59a2c3e55dc7
ashikkhan34 <ashikkhan314167@gmail.com>
2026-06-11 12:29:58 +0600

    nest app

---
+++ src/product/product.service.ts
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ProductService {
+    private products = [
+        {id:1,name:'mobile',price:10000},
+        {id:2,name:'laptop',price:100000},
+        {id:3,name:'tablet',price:1002100},
+    ];
+    getAllProducts(){
+        return this.products;
+    }
+
+    getProductById(id:number){
+        return this.products.find((product)=> product.id === id)
+    }
+}
